79c5b86491ebf4ed7aa2f2fff9cc5e8231aa19f8
Josh Bernitt <jjbernitt@gmail.com>
2016-10-01 12:18:23 -0400

    Working on bottom up DP
    Working on filling in table to give result

--- Longest Palindromic Subsequence.playground/Contents.swift
+++ Longest Palindromic Subsequence.playground/Contents.swift
@@ -1,7 +1,7 @@
 //: Playground - noun: a place where people can play
 
-let name: String = "JOSHUAJAMESBERNITT";
-let test: String = "character";
+var name: String = "JOSHUAJAMESBERNITT";
+var test: String = "character";
 
 class Object {
     var length: Int = -1;
@@ -10,35 +10,43 @@ class Object {
 
 var str: Array = Array("abc".characters);
 
-func lps (string: [String], i: Int, j: Int) -> Object {
-    // base case - if the string is 1 character long, then return itself
-    if i == j {
-        let ret = Object();
-        ret.length = i;
-        ret.sequence = string;
-        return ret;
+func lps (string: [String]) -> [[Int]] {
+    let l: Int = string.count;
+    var c: [[Int]] = Array(repeating: Array(repeatElement(0, count: l)), count: l);
+    
+    // length 1 string is a palindrome in and of itself, so the diagonal is going to 
+    // simply be 1's
+    var k: Int = 0;
+    while k <= l - 1 {
+        c[k][k] = 1;
+        k += 1;
     }
     
-    // base case - if the string is of length 2 and the chars are the same
-    if i + 1 == j  && string[i] == string[j] {
-        let ret = Object();
-        ret.length = j;
-        ret.sequence = string;
-        return ret;
+    var j = 0;
+    // skip the first since it's just a 1 already along the diagonal
+    k = 2;
+    while k <= l {
+        var i = 0;
+        while i < l - k + 1 {
+            j = i + k - 1;
+            if string[i] == string[j] && k == 2 {
+                c[i][j] = 2;
+            } else if string[i] == string[j] {
+                
+            }
+            i += 1;
+        }
+        k += 1;
     }
     
-    if string[i] == string[j] {
-        return lps(string: string, i: i + 1, j: j - 1);
-    }
-    
-    
-    
-    return Object();
+    return c;
 }
 
+//test = "aba";
 var input = test.characters.map { String($0) };
-input = ["A", "A", "A"];
 
-let i = 0;
-let j = input.count - 1;
-lps(string: input, i: i , j: j);
+lps(string: input);
+
+
+
+
