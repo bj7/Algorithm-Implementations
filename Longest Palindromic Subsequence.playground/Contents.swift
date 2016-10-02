869835815d7cf6aff7d53118ec82001f2ecbc2c3
Josh Bernitt <jjbernitt@gmail.com>
2016-10-02 15:18:48 -0400

    Found longest number
    need to find the subsequence

--- Longest Palindromic Subsequence.playground/Contents.swift
+++ Longest Palindromic Subsequence.playground/Contents.swift
@@ -8,41 +8,56 @@ class Object {
     var sequence: Array = [""];
 }
 
-var str: Array = Array("abc".characters);
-
-func lps (string: [String]) -> [[Int]] {
-    let l: Int = string.count;
-    var c: [[Int]] = Array(repeating: Array(repeatElement(0, count: l)), count: l);
+func lps (string: [String]) -> Object {
+    let n: Int = string.count;
+    var c: [[Int]] = Array(repeating: Array(repeatElement(0, count: n)), count: n);
     
     // length 1 string is a palindrome in and of itself, so the diagonal is going to 
     // simply be 1's
-    var k: Int = 0;
-    while k <= l - 1 {
+    var k = 0;
+    while k <= n - 1 {
         c[k][k] = 1;
         k += 1;
     }
     
+    let lg = Object();
     var j = 0;
     // skip the first since it's just a 1 already along the diagonal
-    k = 2;
-    while k <= l {
+    var l = 2;
+    while l <= n {
         var i = 0;
-        while i < l - k + 1 {
-            j = i + k - 1;
+        while i < n - l + 1 {
+            j = i + l - 1;
+            // if two characters are the same, then they are a palindrome of length 2
             if string[i] == string[j] && k == 2 {
                 c[i][j] = 2;
+                if 2 > lg.length {
+                    lg.length = 2;
+                }
+                lg.sequence.append(string[i]);
             } else if string[i] == string[j] {
-                
+                // always add two since it is a palindrome of length 2 + k, where k is the
+                // previous length from the immediate upper left diagonal matrix entry,
+                // essentially the previous longest
+                c[i][j] = c[i+1][j-1] + 2;
+                if c[i][j] > lg.length {
+                    lg.length = c[i][j];
+                }
+                lg.sequence.append(string[i]);
+            } else {
+                // this bubbles up the longest subsequence to be c[0][n-1]
+                c[i][j] = (c[i][j-1] > c[i+1][j] ? c[i][j-1] : c[i+1][j]);
             }
             i += 1;
         }
-        k += 1;
+        l += 1;
     }
     
-    return c;
+    c;
+    return lg;
 }
 
-//test = "aba";
+//test = "hannah";
 var input = test.characters.map { String($0) };
 
 lps(string: input);
